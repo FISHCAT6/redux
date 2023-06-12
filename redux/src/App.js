144eca2016b764664d1fd261ba691ac871d71f65
侯斌 <790190257@qq.com>
2023-06-12 15:08:42 +0800

    响应式修改state值

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,16 +1,16 @@
 import {createContext, useContext, useState} from "react";
-
+//创建上下文进行包裹
 const appContext = createContext(null);
 const User = () => {
+    //useContext接收一个context对象，返回上下文对象的value
     const contextValue = useContext(appContext)
     return <div>User: {contextValue.appState.user.name}</div>
 }
-
 const UserModifier = () => {
-    const contextValue = useContext(appContext)
+    const {appState,setAppState} = useContext(appContext)
     const onChange = (e) => {
-        contextValue.appState.user.name = e.target.value;
-        contextValue.setAppState({...contextValue.appState})
+        appState.user.name = e.target.value;
+        setAppState({...contextValue.appState})
     }
     return <div>
         <input value={contextValue.appState.user.name} onChange={onChange}/>
@@ -18,17 +18,17 @@ const UserModifier = () => {
 }
 const One = () => <div
     style={{width: "800px", height: "100px", margin: "20px auto", backgroundColor: "lightblue", borderRadius: "20px"}}>
-    <section style={{textAlign: "center", fontSize: "30px"}}>One<User/></section>
+    <section style={{textAlign: "center", fontSize: "30px"}}>一级展示<User/></section>
 </div>
 
 const Two = () => <div
     style={{width: "800px", height: "100px", margin: "20px auto", backgroundColor: "lightblue", borderRadius: "20px"}}>
-    <section style={{textAlign: "center", fontSize: "30px"}}>Two<UserModifier/></section>
+    <section style={{textAlign: "center", fontSize: "30px"}}>二级展示<UserModifier/></section>
 </div>
 
 const Three = () => <div
     style={{width: "800px", height: "100px", margin: "20px auto", backgroundColor: "lightblue", borderRadius: "20px"}}>
-    <section style={{textAlign: "center", fontSize: "30px"}}>Three</section>
+    <section style={{textAlign: "center", fontSize: "30px"}}>三级展示</section>
 </div>
 
 function App() {
