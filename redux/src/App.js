d94d0851670950d8aa7a120af7c86edadd3df472
侯斌 <790190257@qq.com>
2023-06-12 11:24:47 +0800

    初始化项目

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,9 +1,39 @@
+import {createContext, useContext, useState} from "react";
+
+const appContext = createContext(null);
+const User = () => {
+    const contextValue = useContext(appContext)
+    return <div>User: {contextValue.appState.user.name}</div>
+}
+
+const UserModifier = () => {
+    const contextValue = useContext(appContext)
+    const onChange = (e) => {
+        contextValue.appState.user.name = e.target.value;
+        contextValue.setAppState({...contextValue.appState})
+    }
+    return <div>
+        <input value={contextValue.appState.user.name} onChange={onChange}/>
+    </div>
+}
+const One = () => <section>One<User/></section>
+
+const Two = () => <section>Two<UserModifier/></section>
+
+const Three = () => <section>Three</section>
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const [appState, setAppState] = useState({
+        user: {name: 'frank', age: 18}
+    })
+    const contextValue = {appState, setAppState}
+    return (
+        <appContext.Provider value={contextValue}>
+            <One></One>
+            <Two></Two>
+            <Three></Three>
+        </appContext.Provider>
+    )
 }
 
 export default App;
